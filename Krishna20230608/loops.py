ff5d29be72d6563d4b5e238bdb5bcbe161d62f21
Krishna Prasad Regmi <kpkrishna309@gmail.com>
2023-06-08 20:19:36 +0530

    Add files via upload

---
+++ Krishna20230608/loops.py
@@ -0,0 +1,19 @@
+#for in in python
+#Write a python program that's print 1 to 10 using for loop
+for i in range(1,11):
+    print(i)
+
+#Write a python program that's print 1 to 10 using for loop same line
+for i in range(1,11):
+    print(i,end=" ")
+
+#Write a above program using while loop
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+#Write a python program to count length of string
+
+mystr = "Krishna"
+print(len(mystr))
